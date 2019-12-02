4e88edc38f8ff58e8ae20246f12ad0b74243a386
mznco <admin@mznco.net>
2019-12-02 15:45:53 -0500

    actually fix sso

--- randomizer/driver.py
+++ randomizer/driver.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.ui import Select
 
 
 LOG_PATH = '/Users/noahvito'
-LOGIN_URL = 'https://{}.slack.com/signin/?no_sso=1'
+LOGIN_URL = 'https://{}.slack.com?no_sso=1'
 GIPHY_URL = 'https://{}.slack.com/services/417362304373'
 
 
@@ -34,6 +34,7 @@ class giphy_war_machine(object):
         print 'DONE!'
 
     def login(self):
+        import ipdb; ipdb.set_trace()
         print 'logging in...'
         self.driver.get(self.login_url)
         email_field = self.driver.find_element_by_id('email')
