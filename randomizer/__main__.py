85ecc2623949716c6248ac7952ca34ea29305135
mznco <admin@mznco.net>
2019-12-02 15:52:08 -0500

    use virtualdisplay

--- randomizer/__main__.py
+++ randomizer/__main__.py
@@ -1,4 +1,4 @@
-# from pyvirtualdisplay import Display
+from pyvirtualdisplay import Display
 
 from driver import giphy_war_machine
 
@@ -6,11 +6,10 @@ password_file = open('password.txt', 'r')
 PASSWORD = password_file.read().strip()
 
 if __name__ == "__main__":
-    # display = Display(visible=0, size=(800, 600))
-    # display.start()
+    display = Display(visible=0, size=(800, 600))
+    display.start()
     try:
         randomizer_5000 = giphy_war_machine(password=PASSWORD)
         randomizer_5000.run()
     finally:
-        pass
-        # display.stop()
+        display.stop()
